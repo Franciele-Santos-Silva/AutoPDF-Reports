c412dea1be8ca3f4e76099fa393b68225aa329b2
Franciele-Santos-Silva <franciellysantos876@gmail.com>
2025-09-25 01:34:29 -0300

    Testando a criação de pdfs

--- main.py
+++ main.py
@@ -1,3 +1,27 @@
+# import pandas as pd
+# from reportlab.pdfgen import canvas
+# from reportlab.lib.pagesizes import A4
+
+# tabela = pd.read_csv('horas_extras.csv')
+# print(tabela)
+
+# nome = 'Franciele Santos da Silva'
+# departamento = 'TI'
+# horas_extras = 12   
+# mes_ref = 'Março/2025'
+
+# documento_pdf = canvas.Canvas(f"Relatório de horas extras - {nome}.pdf", pagesize = A4)
+
+# documento_pdf.setFont("Helvetica-Bold", 12)
+# distancia = int(30)
+# documento_pdf.drawString(100, 700, f"Nome: {nome}")
+# documento_pdf.drawString(100, 700 - 1 * f"Departamento- {departamento}")
+# documento_pdf.drawString(100, 700 - 2 * f"Quantidade de horas extras - {horas_extras}")
+# documento_pdf.drawString(100, 700 - 3 * f"Mês de refêrencia - {mes_ref}")
+# documento_pdf.drawString(100, 700 - 1 *f"Esse relatório foi gerado automaticamente em Python")
+
+# documento_pdf.save()
+
 import pandas as pd
 from reportlab.pdfgen import canvas
 from reportlab.lib.pagesizes import A4
@@ -10,7 +34,15 @@ departamento = 'TI'
 horas_extras = 12   
 mes_ref = 'Março/2025'
 
-documento_pdf = canvas.Canvas(f"Relatório de horas extras - {nome}.pdf", pagesize = A4)
+documento_pdf = canvas.Canvas(f"Relatório de horas extras - {nome}.pdf", pagesize=A4)
 
-documento_pdf.setFont("Helvetica-Bolde", 18)
-documento_pdf.drawString(30, 750, f"Relatório de horas extras - {nome}")
+documento_pdf.setFont("Helvetica-Bold", 12)
+distancia = 30
+
+documento_pdf.drawString(100, 700, f"Nome: {nome}")
+documento_pdf.drawString(100, 700 - 1 * distancia, f"Departamento: {departamento}")
+documento_pdf.drawString(100, 700 - 2 * distancia, f"Quantidade de horas extras: {horas_extras}")
+documento_pdf.drawString(100, 700 - 3 * distancia, f"Mês de referência: {mes_ref}")
+documento_pdf.drawString(100, 700 - 4 * distancia, "Esse relatório foi gerado automaticamente em Python")
+
+documento_pdf.save()

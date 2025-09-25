7f40e483662bc8d6f9e8c835ce0a6e517cf2535b
Franciele-Santos-Silva <franciellysantos876@gmail.com>
2025-09-24 23:17:07 -0300

    Criando péginas

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
-# ollhar a base dados
-# criar um relatorio
-# salvar relatorios
-
 import pandas as pd
 from reportlab.pdfgen import canvas
 from reportlab.lib.pagesizes import A4
+
+tabela = pd.read_csv('horas_extras.csv')
+print(tabela)
+
+nome = 'Franciele Santos da Silva'
+departamento = 'TI'
+horas_extras = 12   
+mes_ref = 'Março/2025'
+
+documento_pdf = canvas.Canvas(f"Relatório de horas extras - {nome}.pdf", pagesize = A4)
+
+documento_pdf.setFont("Helvetica-Bolde", 18)
+documento_pdf.drawString(30, 750, f"Relatório de horas extras - {nome}")

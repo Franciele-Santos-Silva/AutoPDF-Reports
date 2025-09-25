d5b40f857fd9a1744ce4efd94dd9df39f3410681
Franciele-Santos-Silva <franciellysantos876@gmail.com>
2025-09-25 01:43:52 -0300

    Alteração na main

--- main.py
+++ main.py
@@ -1,27 +1,3 @@
-# import pandas as pd
-# from reportlab.pdfgen import canvas
-# from reportlab.lib.pagesizes import A4
-
-# tabela = pd.read_csv('horas_extras.csv')
-# print(tabela)
-
-# nome = 'Franciele Santos da Silva'
-# departamento = 'TI'
-# horas_extras = 12   
-# mes_ref = 'Março/2025'
-
-# documento_pdf = canvas.Canvas(f"Relatório de horas extras - {nome}.pdf", pagesize = A4)
-
-# documento_pdf.setFont("Helvetica-Bold", 12)
-# distancia = int(30)
-# documento_pdf.drawString(100, 700, f"Nome: {nome}")
-# documento_pdf.drawString(100, 700 - 1 * f"Departamento- {departamento}")
-# documento_pdf.drawString(100, 700 - 2 * f"Quantidade de horas extras - {horas_extras}")
-# documento_pdf.drawString(100, 700 - 3 * f"Mês de refêrencia - {mes_ref}")
-# documento_pdf.drawString(100, 700 - 1 *f"Esse relatório foi gerado automaticamente em Python")
-
-# documento_pdf.save()
-
 import pandas as pd
 from reportlab.pdfgen import canvas
 from reportlab.lib.pagesizes import A4
